4914079f7c101d117f06cbcaafdaac3cbab84066
Mustapha Abdulai <mazbandago@gmail.com>
2025-07-12 07:50:13 +0000

    check to see

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,4 @@
 const args = process.argv.slice(2);
-
 if (args.length === 0) {
   console.log("No argument");
 } else if (args.length === 1) {
@@ -8,4 +7,16 @@ if (args.length === 0) {
   console.log(`${args[0]} ${args[1]} Arguments found`);
 } else {
   console.log("Good work done");
+}
+
+if (args.lenth===1){
+    console.log(`${args[0]} Argument found`);
+} else{
+    console.log("ignored");
+}
+
+if (args.length===2){
+    console.log(`${args[0]} ${args[1]} Arguments found`)
+} else {
+    console.log("Never mind");
 }
