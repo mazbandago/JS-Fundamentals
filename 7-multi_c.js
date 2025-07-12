799447a5bd0ba73e7b0fb03255e2b1c9bd584b52
Mustapha Abdulai <mazbandago@gmail.com>
2025-07-12 14:29:19 +0000

    Done with i love c task

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const times = Number(process.argv[2]);
+
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+}
